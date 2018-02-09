97e925bb379f29fa98b9807025a373681edd65b9
Justin Stoddard <Stoddard.j.t@gmail.com>
2018-02-09 15:28:40 -0700

    Commit Main.rbV 0.1.0

--- main.rb
+++ main.rb
@@ -4,4 +4,34 @@
 # git fetch
 # git rebase origin/master
 # git push origin/master
-# Whats up 
+require_relative 'answers.rb'
+
+
+
+def main_menu
+    puts "====================="
+    puts "    EightBall App"
+    puts "====================="
+    puts " [1] Enter EightBall"
+    puts " [2] Exit EightBall"
+    choice = gets.strip.
+
+    case choice
+        when "1"
+            eight_ball
+        when "2"
+            puts "========================="
+            puts "You Have Exited EightBall"
+            puts "========================="
+            exit
+        else
+            puts "Invaild Input"
+    end
+
+end
+
+def eight_ball
+    puts ""
+end
+
+main_menu
