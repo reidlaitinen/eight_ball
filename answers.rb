3ee1c4b341f0caa128db13caa2979187483709d1
Reid Laitinen <reidlaitinen@gmail.com>
2018-02-09 15:20:25 -0700

    finish creating initialize and set/get

--- answers.rb
+++ answers.rb
@@ -3,6 +3,13 @@ class Answers
   def initialize(text)
     @text = text
   end
-  def set_text(text)
 
+  def set_text(text)
+    @text = text
+  end
+
+  def get_text(text)
+    return @text
+  end
+  
 end
